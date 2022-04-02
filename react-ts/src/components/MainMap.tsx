03fca03c14533e996955e3746dc1d127e0a17e5c
retkiewi <58555777+retkiewi@users.noreply.github.com>
2022-04-02 20:31:58 +0200

    post marker

--- react-ts/src/components/MainMap.tsx
+++ react-ts/src/components/MainMap.tsx
@@ -1,55 +1,73 @@
-import { Icon, LatLng, Point } from 'leaflet';
-import * as React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet'  
-import AddIcon from '@mui/icons-material/Add';
-import pinIcon from '../resources/pinIcon2.svg';
-import { Fab, Box } from '@mui/material';
+import { Icon, LatLng, Point } from "leaflet";
+import * as React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  Tooltip,
+} from "react-leaflet";
+import AddIcon from "@mui/icons-material/Add";
+import pinIcon from "../resources/pinIcon2.svg";
+import { Fab, Box } from "@mui/material";
+import { PostMarker } from "./postMarker/postMarker";
 
 function AddButton() {
   return (
-    <Box sx={{ position: 'absolute', bottom: '3rem', right: '3rem' }}>
-      <Fab sx={{width:'5rem', height:'5rem'}}>
-        <AddIcon sx={{width:'50%', height:'50%'}}></AddIcon>
+    <Box sx={{ position: "absolute", bottom: "3rem", right: "3rem" }}>
+      <Fab sx={{ width: "5rem", height: "5rem" }}>
+        <AddIcon sx={{ width: "50%", height: "50%" }}></AddIcon>
       </Fab>
     </Box>
-  )
+  );
 }
 
 function UserMarker() {
-    const userIcon = new Icon({
-        iconUrl: pinIcon,
-        iconSize: new Point(40, 40),  
-      });
-    
-    const [position, setPosition] = useState<LatLng | undefined>();
+  const userIcon = new Icon({
+    iconUrl: pinIcon,
+    iconSize: new Point(40, 40),
+  });
 
-    const map = useMap();
+  const [position, setPosition] = useState<LatLng | undefined>();
 
-    useEffect(() => {
-      map.locate().on("locationfound", function (e) {
-        setPosition(e.latlng);
-        map.flyTo(e.latlng, 15);
-      });
-    }, [map]);
+  const map = useMap();
 
-    return !position ? null :(
-        <Marker position={position} icon={userIcon}></Marker>
-    );
+  useEffect(() => {
+    map.locate().on("locationfound", function (e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, 15);
+    });
+  }, [map]);
+
+  return !position ? null : (
+    <Marker position={position} icon={userIcon}></Marker>
+  );
 }
 
 function MainMap(props: any) {
-    return (
-        <MapContainer style={{height: "100vh"}} center={[52.1064618,18.5525723]} zoom={7}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-        <UserMarker />
-          <AddButton></AddButton>
-        </MapContainer>
-    );                                                                                                          
-}   
+  return (
+    <MapContainer
+      style={{ height: "100vh" }}
+      center={[52.1064618, 18.5525723]}
+      zoom={7}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* <UserMarker /> */}
+      <PostMarker
+        position={new LatLng(52.1064618, 18.5525723)}
+        open
+        children={<h1>Super Post</h1>}
+      />
+
+      <AddButton></AddButton>
+    </MapContainer>
+  );
+}
 
 export default MainMap;
