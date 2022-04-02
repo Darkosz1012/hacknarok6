93d20009b8b244897980370da7a54ff4e9d2547d
Dariusz Biela <darek10121998db@gmail.com>
2022-04-02 14:27:06 +0200

    page signin and singup

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
-    return <Dashboard />;
+    return ( 
+        <Router>
+        <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signup" component={SignUp} />
+        </Switch>
+        </Router>
+    );
 }
 
 export default App;
